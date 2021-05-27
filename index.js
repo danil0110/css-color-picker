b518826827dd53c01788d26ac81903ff4f222e72
danil0110 <danilmail0110@gmail.com>
2021-05-27 23:41:40 +0300

    Added option to change color type

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const body = document.body;
 const btnGenerate = document.querySelector('#btn-generate');
 const colorField = document.querySelector('.css-color span');
+const btnTypeHex = document.querySelector('#type-hex');
+const btnTypeRgb = document.querySelector('#type-rgb');
 
 let currentColor = [20, 30, 40];
+let colorType = 'rgb';
 
 const getRandomNumber = () => {
 	return Math.floor(Math.random() * 256);
@@ -23,9 +26,14 @@ const getRandomColor = () => {
 };
 
 const toHexColor = rgbArray => '#' + rgbArray.map(toHexNumber).join('');
+const toRgbColor = rgbArray => 'rgb(' + rgbArray.join(', ') + ')';
 
 const updateColorField = () => {
-	colorField.textContent = toHexColor(currentColor);
+	if (colorType === 'hex') {
+		return (colorField.textContent = toHexColor(currentColor));
+	}
+
+	colorField.textContent = toRgbColor(currentColor);
 };
 
 const updateBodyBackground = () => {
@@ -36,6 +44,22 @@ btnGenerate.addEventListener('click', () => {
 	currentColor = getRandomColor();
 	updateColorField();
 	updateBodyBackground();
+});
+
+btnTypeHex.addEventListener('click', () => {
+	btnTypeHex.classList.add('active');
+	btnTypeRgb.classList.remove('active');
+
+	colorType = 'hex';
+	updateColorField();
+});
+
+btnTypeRgb.addEventListener('click', () => {
+	btnTypeRgb.classList.add('active');
+	btnTypeHex.classList.remove('active');
+
+	colorType = 'rgb';
+	updateColorField();
 });
 
 document.addEventListener('keypress', e => {
