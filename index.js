1a9b8811af50d9b865af249a6bcd172a0e3a1962
danil0110 <danilmail0110@gmail.com>
2021-05-27 23:22:37 +0300

    Color-picker works fine

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const body = document.body;
+const btnGenerate = document.querySelector('#btn-generate');
+const colorField = document.querySelector('.css-color span');
+
+let currentColor = [20, 30, 40];
+
+const getRandomNumber = () => {
+	return Math.floor(Math.random() * 256);
+};
+
 const toHexNumber = number => {
 	let hex = number.toString(16);
 
@@ -8,4 +18,30 @@ const toHexNumber = number => {
 	return hex;
 };
 
-const toHexColor = arr => '#' + arr.map(toHexNumber).join('');
+const getRandomColor = () => {
+	return [0, 0, 0].map(() => getRandomNumber());
+};
+
+const toHexColor = rgbArray => '#' + rgbArray.map(toHexNumber).join('');
+
+const updateColorField = () => {
+	colorField.textContent = toHexColor(currentColor);
+};
+
+const updateBodyBackground = () => {
+	body.style.backgroundColor = toHexColor(currentColor);
+};
+
+btnGenerate.addEventListener('click', () => {
+	currentColor = getRandomColor();
+	updateColorField();
+	updateBodyBackground();
+});
+
+document.addEventListener('keypress', e => {
+	if (e.key === ' ') {
+		currentColor = getRandomColor();
+		updateColorField();
+		updateBodyBackground();
+	}
+});
